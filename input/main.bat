4450d7b405cd1e690aac090603f0b642cae58492
iori3150 <iori3150jp@gmail.com>
2024-07-19 22:12:45 +0900

    reset

--- input/main.bat
+++
@@ -1,8 +0,0 @@
-del result\*.prof
-.\main\main.exe 2>result\error.log
-
-del prof2vtu\prof2vtu.exe
-g++ prof2vtu\prof2vtu.cpp -std=c++17  -o prof2vtu\prof2vtu.exe
-
-del result\*.vtu
-.\prof2vtu\prof2vtu.exe 2>result\error.log
