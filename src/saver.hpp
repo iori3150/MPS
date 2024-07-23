4322b360d6297a1eb1eae1c9c5bd9c822f8758f9
iori3150 <iori3150jp@gmail.com>
2024-07-23 12:07:52 +0900

    Update Vtk Conversion

--- src/saver.hpp
+++ src/saver.hpp
@@ -13,6 +13,15 @@ public:
     Saver(const std::filesystem::path& outputDir);
 
     void save(const std::vector<Particle>& particles, const double& time);
+    void toCsv(const std::vector<Particle>& particles, const double& time);
+    void toVtu(const std::vector<Particle>& particles, const double& time);
+    void dataArrayBegin(
+        std::ofstream& outFile,
+        const std::string& numberOfComponents,
+        const std::string& type,
+        const std::string& name
+    );
+    void dataArrayEnd(std::ofstream& outFile);
 
 private:
     std::filesystem::path outputDir;
