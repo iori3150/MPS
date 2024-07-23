5bbcaff1b9efb64cdb15f0a7e35ab3c2ad609e30
iori3150 <iori3150jp@gmail.com>
2024-07-23 10:10:26 +0900

    Update using namespace

--- src/saver.cpp
+++ src/saver.cpp
@@ -5,6 +5,9 @@
 #include <format>
 #include <fstream>
 
+using std::format;
+using std::make_tuple;
+
 Saver::Saver(const std::filesystem::path& outputDir) {
     this->outputDir = outputDir;
     std::filesystem::create_directory(outputDir / "csv");
@@ -13,19 +16,18 @@ Saver::Saver(const std::filesystem::path& outputDir) {
 void Saver::save(
     const std::vector<Particle>& particles, const double& time, const int& fileNum
 ) {
-    std::string filename =
-        std::format("{}/csv/output_{:04}.csv", outputDir.string(), fileNum);
+    std::string filename = format("{}/csv/output_{:04}.csv", outputDir.string(), fileNum);
 
     std::ofstream outFile(filename);
     if (!outFile.is_open()) {
-        std::cerr << std::format("Could not open result file: {}", filename) << std::endl;
+        std::cerr << format("Could not open result file: {}", filename) << std::endl;
         exit(-1);
     }
     auto writer = csv::make_csv_writer(outFile);
 
-    writer << std::make_tuple("Time (s)", time);
-    writer << std::make_tuple("Number of Particles", particles.size());
-    writer << std::make_tuple(
+    writer << make_tuple("Time (s)", time);
+    writer << make_tuple("Number of Particles", particles.size());
+    writer << make_tuple(
         "ID",
         "Type",
         "Position.x (m)",
@@ -42,17 +44,17 @@ void Saver::save(
             continue;
         }
 
-        writer << std::make_tuple(
-            std::format("{:4}", pi.id),
-            std::format("{:2d}", static_cast<int>(pi.type)),
-            std::format("{:8.3f}", pi.position.x()),
-            std::format("{:8.3f}", pi.position.y()),
-            std::format("{:8.3f}", pi.position.z()),
-            std::format("{:8.3f}", pi.velocity.x()),
-            std::format("{:8.3f}", pi.velocity.y()),
-            std::format("{:8.3f}", pi.velocity.z()),
-            std::format("{:9.3f}", pi.pressure),
-            std::format("{:8.3f}", pi.numberDensity)
+        writer << make_tuple(
+            format("{:4}", pi.id),
+            format("{:2d}", static_cast<int>(pi.type)),
+            format("{:8.3f}", pi.position.x()),
+            format("{:8.3f}", pi.position.y()),
+            format("{:8.3f}", pi.position.z()),
+            format("{:8.3f}", pi.velocity.x()),
+            format("{:8.3f}", pi.velocity.y()),
+            format("{:8.3f}", pi.velocity.z()),
+            format("{:9.3f}", pi.pressure),
+            format("{:8.3f}", pi.numberDensity)
         );
     }
 
