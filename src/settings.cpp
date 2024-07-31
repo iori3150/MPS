c9a24cfa597dc9ca38d3d0b465ee8cc84836ffc8
iori3150 <iori3150jp@gmail.com>
2024-07-31 22:25:36 +0900

    Change settings.yml

--- src/settings.cpp
+++ src/settings.cpp
@@ -20,35 +20,35 @@ void Settings::load() {
 
     try {
         dim              = root["dim"].get_value<int>();
-        particleDistance = root["particleDistance"].get_value<double>();
+        particleDistance = root["particle distance"].get_value<double>();
         dt               = root["dt"].get_value<double>();
-        finishTime       = root["finishTime"].get_value<double>();
-        outputInterval   = root["outputInterval"].get_value<double>();
-        cflCondition     = root["cflCondition"].get_value<double>();
+        finishTime       = root["finish time"].get_value<double>();
+        outputInterval   = root["output interval"].get_value<double>();
+        cflCondition     = root["CFL condition"].get_value<double>();
 
-        double xMin = root["domainRangeX"][0].get_value<double>();
-        double xMax = root["domainRangeX"][1].get_value<double>();
-        double yMin = root["domainRangeY"][0].get_value<double>();
-        double yMax = root["domainRangeY"][1].get_value<double>();
-        double zMin = root["domainRangeZ"][0].get_value<double>();
-        double zMax = root["domainRangeZ"][1].get_value<double>();
+        double xMin = root["domain range X"][0].get_value<double>();
+        double xMax = root["domain range X"][1].get_value<double>();
+        double yMin = root["domain range Y"][0].get_value<double>();
+        double yMax = root["domain range Y"][1].get_value<double>();
+        double zMin = root["domain range Z"][0].get_value<double>();
+        double zMax = root["domain range Z"][1].get_value<double>();
         domain      = Domain(xMin, xMax, yMin, yMax, zMin, zMax);
 
         density            = root["density"].get_value<double>();
-        kinematicViscosity = root["kinematicViscosity"].get_value<double>();
+        kinematicViscosity = root["kinematic viscosity"].get_value<double>();
 
         gravity.x() = root["gravity"][0].get_value<double>();
         gravity.y() = root["gravity"][1].get_value<double>();
         gravity.z() = root["gravity"][2].get_value<double>();
 
         effectiveRadius.pressure =
-            root["effectiveRadiusRatio"]["pressure"].get_value<double>() *
+            root["effective radius ratio"]["pressure"].get_value<double>() *
             particleDistance;
         effectiveRadius.viscosity =
-            root["effectiveRadiusRatio"]["viscosity"].get_value<double>() *
+            root["effective radius ratio"]["viscosity"].get_value<double>() *
             particleDistance;
         effectiveRadius.surfaceDetection =
-            root["effectiveRadiusRatio"]["surfaceDetection"].get_value<double>() *
+            root["effective radius ratio"]["surface detection"].get_value<double>() *
             particleDistance;
         effectiveRadius.max = std::max(
             {effectiveRadius.pressure,
@@ -57,15 +57,15 @@ void Settings::load() {
         );
 
         thresholdForSurfaceDetection =
-            root["thresholdForSurfaceDetection"].get_value<double>();
+            root["threshold for surface detection"].get_value<double>();
 
         compressibility = root["compressibility"].get_value<double>();
         relaxationCoefficientForPressure =
-            root["relaxationCoefficientForPressure"].get_value<double>();
+            root["relaxation coefficient for pressure"].get_value<double>();
 
         collisionDistance =
-            root["collisionDistanceRatio"].get_value<double>() * particleDistance;
-        coefficientOfRestitution = root["coefficientOfRestitution"].get_value<double>();
+            root["collision distance ratio"].get_value<double>() * particleDistance;
+        coefficientOfRestitution = root["coefficient of restitution"].get_value<double>();
 
     } catch (const fkyaml::exception& e) {
         std::cerr << e.what() << std::endl;
