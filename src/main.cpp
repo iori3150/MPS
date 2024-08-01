968359efce525927308b324287e9b05d9800cef4
iori3150 <iori3150jp@gmail.com>
2024-08-01 17:05:22 +0900

    small

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,15 @@
 #include "simulation.hpp"
 
 #include <filesystem>
+#include <iostream>
 
 int main() {
     std::filesystem::path inputYamlPath = "input/settings.yml";
+    if (!std::filesystem::exists(inputYamlPath)) {
+        std::cout << "Input YAML file cannot be found in specified path: "
+                  << inputYamlPath << std::endl;
+        exit(-1);
+    }
 
     Simulation simulation;
     simulation.run(inputYamlPath);
