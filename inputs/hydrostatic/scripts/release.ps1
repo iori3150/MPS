b2b1f52272c340c5214657721fc3a3ba114c918b
Iori Saigo <88538282+iori3150@users.noreply.github.com>
2024-08-14 15:18:10 +0900

    91 make makefiles for inputs (#98)
    * Create Makefile for hydrostatic
    
    * Create Makefile for dambreak

--- inputs/hydrostatic/scripts/release.ps1
+++
@@ -1,4 +0,0 @@
-clang++ -std=c++20 -I ../../submodules/eigen -c main.cpp -o build/main.o
-clang++ -std=c++20 -I ../../submodules/eigen -c ../../src/particle.cpp -o build/particle.o
-clang++ -std=c++20 -I ../../submodules/eigen -I ../../submodules/csv-parser/single_include -I ../../submodules/spdlog/include -c ../../src/exporter.cpp -o build/exporter.o
-clang++ build/main.o build/particle.o build/exporter.o -o build/main.exe
